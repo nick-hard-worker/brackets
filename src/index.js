9b6399e86af12abbd6d5702317c30c5fd5b37f59
NickUA <246246s@gmail.com>
2020-10-25 10:09:08 +0200

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length === 0 || str.length === 1) return false;
+
+  //transform bracketsConfig to object
+  const pares = {};
+  bracketsConfig.forEach(element => {
+    if (element[0] === element[1]) pares[element[0]] = true
+    else pares[element[0]] = element[1];
+  });
+
+  const stack = [str[0]];
+  for (let i = 1; i <= str.length; i++) {
+    const leftBr = stack[stack.length - 1];
+
+    // if pare is the same symbol
+    if (pares[leftBr] === true) {
+      if (leftBr === str[i]) {
+        stack.pop();
+        continue;
+      }
+      else {
+        stack.push(str[i]);
+        continue;
+      }
+    }
+
+    if (isOpenBracket(str[i])) stack.push(str[i])
+    else {
+      if (pares[leftBr] === str[i]) stack.pop()
+      else return false
+    }
+  }
+
+  if (stack.length === 0) return true
+  return false;
+
+  function isOpenBracket(char) {
+    if (pares[char]) return true
+    return false
+  }
 }
